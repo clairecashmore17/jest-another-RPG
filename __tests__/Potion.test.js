3a603ed53d37754b9403ae7e9ad02bcae631dc7a
Claire Cashmore <clairecashmore2017@gmail.com>
2022-07-18 19:32:05 -0400

    added Game initialization

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -8,3 +8,10 @@ expect(potion.name.length).toBeGreaterThan(0);
 expect(potion.value).toEqual(expect.any(Number));
 
 });
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+  
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+  });
